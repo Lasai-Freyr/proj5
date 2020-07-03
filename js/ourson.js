86d429de975b2e7009d58ae8ad78e1c3331f8404
Lasai-Freyr <ferry.thomas@hotmail.fr>
2020-07-03 17:30:12 +0200

    première ébauche du site

---
+++ js/ourson.js
@@ -0,0 +1,8 @@
+
+
+function changeImage(liste)
+{
+    var valeur = document.getElementById("listcouleur").value;
+    document.getElementById('image').src="./images/teddy_"+valeur+"b.jpg";
+    
+}   
