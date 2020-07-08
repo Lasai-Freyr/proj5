dd276e2d35c6cbf38344b4779397bf8ac4554749
Lasai-Freyr <ferry.thomas@hotmail.fr>
2020-07-08 17:48:50 +0200

    réalisation js page ourson

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -15,24 +15,29 @@ function getProducts(){
 
 function displayProducts(){
     console.log("products contient "+products.length+" éléments");
-    let teddyList = "<ul classe='products list-unstyled'>";
+    let teddyList = "<ul classe='products '>";    
     console.log('pinguin');
+    
     for (let i=0;i<products.length;i++){
         console.log('pinguin2');
+        
         console.log(products)
-        let product=products[i];        
-        teddyList +='<li>';
-        teddyList += '<div class="card border-success  mb-3 mb-md-5">';
-        teddyList +='<a href="ourson.html" class="stretched-link"><img src="../images/teddy_'+[i+1]+'b.jpg"></a>';
+        let product=products[i];       
+        teddyList +='<li class="list-unstyled list'+i+' ">';
+        teddyList += '<div class="card border-success col-6  mb-3 mb-md-5">';
+        teddyList +='<a href="ourson.html?_id='+product._id+'" class="stretched-link" id="lien"><img src="../images/teddy_'+[i+1]+'b.jpg"></a>';        
+        teddyList+='</form>';
         teddyList +='<div class="card-body">';
         teddyList +='<h5 class="card-title">'+product.name;
         teddyList +='</h5>';
-        teddyList += '<p class="card-text text-break">';
-        teddyList +='Magnifique Ourson en peluche fait main.';
+        teddyList += '<p class="card-text ">';
+        teddyList +=product.price+"€<br>";
+        teddyList +=product.description;
         teddyList +='</p>';
         teddyList +='</div>';
         teddyList +='</div>';
-        teddyList +='</li>';       
+        teddyList +='</li>'; 
+       
     }
     teddyList +='</ul>';
     document.getElementById('productsListing').innerHTML =  teddyList;
--- front-end/js/ourson.js
+++ front-end/js/ourson.js
@@ -1,8 +1,58 @@
+const params = new URLSearchParams(window.location.search);
+let identifiant = params.get('_id');
+let products=[];
+let found=[];
+listcouleur = document.getElementById("listcouleur");
+console.log(identifiant);
 
 
-function changeImage(liste)
-{
-    var valeur = document.getElementById("listcouleur").value;
-    document.getElementById('image').src="./images/teddy_"+valeur+"b.jpg";
+getProducts();  
+
+function getProducts(){
+    let req = new XMLHttpRequest();
+    req.open("GET","http://localhost:3000/api/teddies");
+    req.addEventListener("load", () => {
+        products = JSON.parse(req.responseText);
+        console.log(products);
+        for(let i=0; i<products.length;i++){
+           let product = products[i];
+           if (product._id==identifiant){
+                found=product;
+               console.log("boucle en cours");
+          }
+        }        
+        console.log(found);
+        displayProducts();
+        
+    });
+    req.setRequestHeader("Content-Type","application/json;charset=UTF-8");
+    req.send();
+} 
+
+async function displayProducts(){
     
-}   
+   
+    let content ='<div  class="col-12 col-md-6">';
+    content+='<div class="image">';
+    content+='<img src="'+found.imageUrl+'">';
+    content+='</div>';
+    content +='<p>'+found.name;
+    content +='<br>'+found.price+'€';
+    content+='<br>'+found.description+'</p></div>';
+
+    
+    document.getElementById('shit').innerHTML = content;
+    console.log('remplissage');
+    listing();
+}
+function listing(){
+    let list=[] = found.colors;
+    console.log('list des options : '+list);
+    for (let i =0;i<list.length;i++){
+        let color = list[i];
+        let opt = document.createElement("option");
+        opt.textContent=color;
+        opt.value=color;
+        listcouleur.appendChild(opt);
+    }
+}
